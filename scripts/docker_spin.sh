b1947e8872dfe5ce91b5217f1c655989b5cfe8d7
Sean Franey <sean.b.franey@gmail.com>
2021-05-09 11:24:01 -0700

    Allow overriding CMD (have to put in double quotes)

--- scripts/docker_spin.sh
+++ scripts/docker_spin.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -e TZ=America/Los_Angeles --net=host -v /home/sean/cooking_blog:/cooking_blog --user 1000:1000 hugo-alpine:1.0
+docker run --rm -e TZ=America/Los_Angeles --net=host -v /home/sean/cooking_blog:/cooking_blog --user 1000:1000 hugo-alpine:1.0 $1
