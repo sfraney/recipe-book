24ecb867d255ee3e8ff7c27f0bafdea70ca4abe7
Sean Franey <s_franey@hotmail.com>
2021-12-08 22:03:14 -0800

    Fix script pointer

--- scripts/docker_spin.sh
+++ scripts/docker_spin.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -e TZ=America/Los_Angeles --net=host -v /home/sean/Documents/blogs/RecipeBook:/blogs --user 1000:1000 hugo-alpine:1.0 $1
+docker run --rm -e TZ=America/Los_Angeles --net=host -v /home/sean/Documents/blogs/recipe-book:/blogs --user 1000:1000 hugo-alpine:1.0 $1
