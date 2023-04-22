7b940fe01d1c806f19dc5fd29721162feb5320fe
Sean Franey <s_franey@hotmail.com>
2023-04-22 16:46:57 -0700

    Fix scripts for new docker names

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,7 +12,7 @@ cd $BASEDIR
 pwd
 
 # Spin up docker to generate static pages w/ Hugo
-#docker run --rm -e TZ=America/Los_Angeles --net=host -v /home/sean/cooking_blog:/cooking_blog --user 1000:1000 hugo-alpine:1.0
+#docker run --rm -e TZ=America/Los_Angeles --net=host -v /home/sean/cooking_blog:/cooking_blog --user 1000:1000 hugo:latest
 $DIR/docker_spin.sh hugo
 
 # Commit generated pages
